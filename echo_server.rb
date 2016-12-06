261897a7ae3b930f0b4b8372de10b00d61645219
MatthijsBrink <m.brink@nerds.company>
2016-12-06 09:00:50 +0100

    Initial commit

---
+++ echo_server.rb
@@ -0,0 +1,33 @@
+require 'eventmachine'
+require 'websocket-eventmachine-server'
+require 'hue'
+require 'sonos'
+
+PORT = (ARGV.shift || 8080).to_i
+
+EM::run do
+  @channel = EM::Channel.new
+
+  puts "start websocket server - port:#{PORT}"
+
+  WebSocket::EventMachine::Server.start(host: '0.0.0.0', port: PORT) do |ws|
+    ws.onopen do
+      sid = @channel.subscribe do |mes|
+        ws.send mes
+      end
+      puts "<#{sid}> connect"
+
+      @channel.push "hello new client <#{sid}>"
+
+      ws.onmessage do |msg|
+        puts "<#{sid}> #{msg}"
+      end
+
+      ws.onclose do
+        puts "<#{sid}> disconnected"
+        @channel.unsubscribe sid
+        @channel.push "<#{sid}> disconnected"
+      end
+    end
+  end
+end
